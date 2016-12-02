bc527dacc55afa1822403fad5b937072a05577bc
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-12-02 11:34:53 +0000

    Add script to copy the debian files
    This new script will copy the koha-* scripts and the docs from the git
    repo to the place they should go in the system

---
+++ cp_debian_files.pl
@@ -0,0 +1,29 @@
+use Modern::Perl;
+use File::Basename qw( dirname );
+use Cwd 'abs_path';
+use IPC::Cmd qw( run );
+
+my $koha_dir = '/home/vagrant/kohaclone'; # FIXME hardcoded
+my $koha_debian_dir = "$koha_dir/debian";
+
+open my $fh, '<', $koha_debian_dir . '/koha-common.install' or die "Cannnot open file $!";
+
+while ( my $line = <$fh> ) {
+    chomp $line;
+    $line =~ s|\s+| |;
+    my ( $from, $to ) = split ' ', $line;
+    next unless $to; # TODO We could handle that
+    next if $from =~ m|/tmp/|;
+    next if $from =~ m|/tmp_docbook/|; # Done later
+    $to = "/$to";
+    my $cmd = "sudo cp $koha_dir/$from $to";
+    run( command => $cmd, verbose => 1 );
+}
+
+close $fh;
+
+run( command => "sudo xsltproc --output /usr/share/man/man8/ /usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl $koha_debian_dir/docs/*.xml", verbose => 1 );
+run( command => "sudo rm /usr/share/man/man8/koha-*.8.gz", verbose => 1 );
+run( command => "sudo gzip /usr/share/man/man8/koha-*.8", verbose => 1 );
+
+exit(0);
