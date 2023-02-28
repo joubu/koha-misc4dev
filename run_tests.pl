96d33875a868d9257efa66228d33a4dea1a0d28f
Jonathan Druart <jonathan.druart@gmail.com>
2023-02-28 12:47:49 +0100

    Issue #62 - Run cypress as the Koha user
    See ktd #355 https://gitlab.com/koha-community/koha-testing-docker/-/issues/355

--- run_tests.pl
+++ run_tests.pl
@@ -220,7 +220,8 @@ sub build_prove_command {
 sub build_cypress_command {
     my ($params) = @_;
     my $env = $params->{env};
-    return sprintf q{yarn cypress run --config video=false,screenshotOnRunFailure=false --env KOHA_USER=%s,KOHA_PASS=%s}, $env->{KOHA_USER}, $env->{KOHA_PASS};
+    return sprintf q{koha-shell %s -c 'yarn cypress run --config video=false,screenshotOnRunFailure=false --env KOHA_USER=%s,KOHA_PASS=%s'},
+        $instance, $env->{KOHA_USER}, $env->{KOHA_PASS};
 }
 
 sub get_commands_to_reset_db {
