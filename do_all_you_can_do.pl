9ee07482e1086776b9b566ca58f155ecc3435697
Jonathan Druart <jonathan.druart@gmail.com>
2023-12-05 13:52:26 +0100

    skip_yarn_build

--- do_all_you_can_do.pl
+++ do_all_you_can_do.pl
@@ -118,17 +118,17 @@ exit(1) unless $success;
 
 my $version = get_version();
 $version =~ s|\.||g;
-if ( $version >= 220600079 ) {
-    $cmd = "grep -q watch_js $koha_dir/package.json";
-    ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) = run( command => $cmd, verbose => 1 );
-    my $build_command = $success
-        ? 'build_js' # watch_js exists, run it
-        : 'build';   # Assume we have build that will build js and css
-
-    $cmd = "sudo koha-shell $instance -c '(cd $koha_dir ; PATH=$PATH yarn $build_command)'";
-    ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) = run( command => $cmd, verbose => 1 );
-    exit(1) unless $success;
-}
+#if ( $version >= 220600079 ) {
+#    $cmd = "grep -q watch_js $koha_dir/package.json";
+#    ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) = run( command => $cmd, verbose => 1 );
+#    my $build_command = $success
+#        ? 'build_js' # watch_js exists, run it
+#        : 'build';   # Assume we have build that will build js and css
+#
+#    $cmd = "sudo koha-shell $instance -c '(cd $koha_dir ; PATH=$PATH yarn $build_command)'";
+#    ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) = run( command => $cmd, verbose => 1 );
+#    exit(1) unless $success;
+#}
 
 # Rebuild Elastic
 if ($elasticsearch) {
